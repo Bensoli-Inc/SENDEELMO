999ffde8604978c5e82421be338449c5841c466a
Bensolidev <77503329+Bensoli-Inc@users.noreply.github.com>
2023-11-27 17:05:21 +0300

    Update App.jsx
    edited app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import './App.css'
-import Partners from "./components/Partners/Partners";
 import Contact from "./components/Contact/Contact";
 import Booking from "./components/Booking/Booking";
 import Footer from "./components/Footer/Footer";
+
 function App() {
   return (
     <div className="App">
@@ -13,11 +13,9 @@ function App() {
         <Header />
         <Home /> 
       </div>
-      <Partners />
       <Contact />
       <Booking />
       <Footer />
-
     </div>
   );
 }
