fd24e62713c0fbd818c9196e761c12da433136bc
Bensolidev <77503329+Bensoli-Inc@users.noreply.github.com>
2023-12-01 00:14:24 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
     return (
          <section className="h-wrapper">
             <div className="flexCenter innerWidth paddings h-container">
-                <img src="./logo.png" alt="logo"  width={100}/>
+                <img src="public./logo.png" alt="logo"  width={100}/>
                 
             <OutsideClickHandler
             onOutsideClick={() => {
