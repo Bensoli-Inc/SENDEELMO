35264a19282c7947c66cbda05600a05f20014c77
Bensolidev <77503329+Bensoli-Inc@users.noreply.github.com>
2023-12-01 01:15:29 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,18 @@ import './App.css'
 import Contact from "./components/Contact/Contact";
 import Booking from "./components/Booking/Booking";
 import Footer from "./components/Footer/Footer";
+import Back from "./components/back/back";
 
 function App() {
   return (
     <div className="App">
       <div>
-        
-          <Header />
-        <div className="white-gradient" />
-          <Home /> 
-       
-      </div>
+      
+      <Header />
+      <div className="white-gradient" />
+      <Home />
+    </div>
+      <Back />
       <Contact />
       <Booking />
       <Footer />
