54bc3b6a9fdc0180e7f38be019b693e3ac5e316d
Bensolidev <77503329+Bensoli-Inc@users.noreply.github.com>
2023-12-01 01:14:57 +0300

    Add files via upload

---
+++ src/components/Back/Back.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Back.css'
+
+const Back = () => {
+    return (
+        <section>
+            <div className="backk">
+                mimi
+            </div>
+        </section>
+    )
+}
+
+export default Back
