17c5ce0b99c14a67a4c6b8f2d2e8cc21261b4444
Bensolidev <77503329+Bensoli-Inc@users.noreply.github.com>
2023-11-30 22:12:07 +0300

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,12 +6,12 @@ import {motion} from 'framer-motion'
 
 const Home = () => {
     return (
-        
-            <div className="paddings innerWidth hero-container ">
+            <section>
+            <div className="flexColStart paddings innerWidth hero-container ">
                  
-{/*left side */}
+
                 <div className="flexColStart hero-left " >
-                    <div className="hero-title">
+                    <div className="hero-title title1">
                         <div className="orange-circle" />
                         <motion.h1
                         initial={{y: "2rem", opacity: 0}}
@@ -27,56 +27,47 @@ const Home = () => {
                     </div>
 
                     <div className=" flexColStart">
-                        <span className='secondaryText'>Contact us for new Electrical & Solar panel installations, all fault diagnosis, maintenance & electrical repairs</span>
-                        <span className='secondaryText'>We also set up Electric fences & CCTV cameras and their maintenance</span>
+                        <span className=' empower'>We empower homes and businesses with seamless electrical </span>
+                        <span className='empower'>solutions that prioritize safety, efficiency, and innovation.</span>
                     </div>
                     
                 
-{/* STATS */}
-                <div className="flexCenter stats">
-                    <div className=" flexColCenter stat">
-                       <span>
-                        <CountUp start={8800} end={9000} duration={4}/>
-                        <span>+</span>
-                       </span>
-                       <span className='secondaryText'>Installations</span> 
-                    </div>
-
-                    <div className="flexColCenter stat">
-                       <span>
-                        <CountUp start={1950} end={2000} duration={4}/>
-                        <span>+</span>
-                       </span>
-                       <span className='secondaryText'>Happy Clients</span> 
-                    </div>
-
-                    <div className="flexColCenter stat">
-                       <span>
-                        <CountUp end={28} />
-                        <span>+</span>
-                       </span>
-                       <span className='secondaryText'>Award Winnings</span> 
-                    </div>
-                </div>
-                </div>
-
-
-{/*right side */}
-                <div className="flexCenter hero-right">
-                    <motion.div 
-                    initial={{x: "7rem", opacity: 0}}
-                    animate={{x: 0, opacity: 1 }}
-                    transition={{
-                        duration: 2,
-                        type: "spring"
-                    }}
                     
-                    className="image-container ">
-                        <img src="./home.jpeg" alt="" />
-                    </motion.div>
+                    <div className="flexCenter stats">
+                        <div className=" flexColCenter stat">
+                        <span>
+                            <CountUp start={8800} end={9000} duration={4}/>
+                            <span>+</span>
+                        </span>
+                        <span className='secondaryText'>Installations</span> 
+                        </div>
+
+                        <div className="flexColCenter stat">
+                        <span>
+                            <CountUp start={1950} end={2000} duration={4}/>
+                            <span>+</span>
+                        </span>
+                        <span className='secondaryText'>Happy Clients</span> 
+                        </div>
+
+                        <div className="flexColCenter stat">
+                        <span>
+                            <CountUp end={28} />
+                            <span>+</span>
+                        </span>
+                        <span className='secondaryText'>Award Winnings</span> 
+                        </div>
+                    </div>
+                    
                 </div>
+                <div>
+                <div className="white-gradient light"/>
+                </div>
+               
+                
             </div>
-        
+            
+            </section>           
     )
 }
 
