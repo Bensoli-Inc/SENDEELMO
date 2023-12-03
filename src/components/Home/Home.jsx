29484f6257e40fb4520754b2c928db4563181337
Bensolidev <77503329+Bensoli-Inc@users.noreply.github.com>
2023-12-03 16:24:56 +0300

    Update Home.jsx
    updated home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import './Home.css';
-import {HiLocationMarker} from 'react-icons/hi';
-import CountUp from 'react-countup';
+import '@/components/Contact/Contact.css';
 import {motion} from 'framer-motion'
+import {MdCall} from 'react-icons/md'
+import {BsFillChatDotsFill} from 'react-icons/bs'
+
+function openWhatsApp () {
+    const phoneNumber = "+254790998545"
+    const whatsappLink = "https://wa.me/+254790998545"
+    window.open(whatsappLink)
+}
+
+
+function callMe () {
+        const phoneNumber = "+254790998545"
+        const telLink = "tel:" +254790998545;
+        window.location.href = telLink;
+    }
 
 const Home = () => {
     return (
@@ -10,12 +24,9 @@ const Home = () => {
             <div className="paddings innerWidth hero-container ">
                  
 
-                <div className="flexColStart hero-left " >
+                <div className="flexCenter hero-left " >
             
                     <div className="hero-title title1">
-        
-                    
-                       
                         <motion.h1
                         initial={{y: "2rem", opacity: 0}}
                         animate={{y: 0, opacity: 1}}
@@ -28,48 +39,56 @@ const Home = () => {
                             Engineering 
                             <br /> Services
                         </motion.h1>
-                    </div>
-
-                    <div className=" flexColStart">
-                        <span className=' secondaryText'> Hire us for all Electrical, Security, &  </span>
-                        <span className='secondaryText'>Maintenance Operations.</span>
-                    </div>
-                    
-                
-                    
-                    <div className="flexCenter stats">
-                        <div className=" flexColCenter stat">
-                        <span>
-                            <CountUp start={800} end={1000} duration={4}/>
-                            <span>+</span>
-                        </span>
-                        <span className='secondaryText'>Installations</span> 
-                        </div>
-
-                        <div className="flexColCenter stat">
-                        <span>
-                            <CountUp start={150} end={200} duration={4}/>
-                            <span>+</span>
-                        </span>
-                        <span className='secondaryText'>Happy Clients</span> 
-                        </div>
-
-                        <div className="flexColCenter stat">
-                        <span>
-                            <CountUp end={28} />
-                            <span>+</span>
-                        </span>
-                        <span className='secondaryText'>Award Winnings</span> 
-                        </div>
-                        
-                    </div>
-                    
+                    </div>  
+                    <div className=" flexColCenter service">
+                        <li>&#10003; <span className="secondaryText">Wiring connection</span></li>
+                        <li>&#10003; <span className="secondaryText">CCTV installation</span></li>
+                        <li>&#10003; <span className="secondaryText">Solar Panel installation</span></li>
+                        <li>&#10003; <span className="secondaryText">Electric fence installation</span></li>
+                        <li>&#10003; <span className="secondaryText">Fault diagnosis & maintenance</span></li>
+                    </div>           
                 </div>
 
-                <div className='rait'>
+                <div className='rait'>                   
+                    {/*<div className="white-gradient light"/> */}
                     
-                    <div className="white-gradient light"/>
-                    
+                    <div className="flexColCenter contactModes">
+
+                        {/*first row*/}
+                        <div className="flexColCenter row">
+                            <div className="flexColCenter mode">
+                                <div className="flexStart">
+                                    <div className="flexCenter icon">
+                                        <MdCall size={25}/>
+                                    </div>
+                                    <div className="flexColStart detail">
+                                        <span className="primaryText">Call</span>
+                                        <span className="secondaryText">0790998545</span>
+                                    </div>
+                                </div>
+                                <div className="flexCenter button" onClick={callMe}>
+                                    Call Us
+                                </div>
+                            </div>
+
+                            {/*second mode*/}
+                            <div className="flexColCenter mode">
+                                <div className="flexStart">
+                                <div className="flexCenter icon">
+                                        <BsFillChatDotsFill size={25}/>
+                                    </div>
+                                    <div className="flexColStart detail">
+                                        <span className="primaryText">Whatsapp</span>
+                                        <span className="secondaryText">0790998545</span>
+                                    </div>
+                                </div>
+                                <div className="flexCenter button" onClick={openWhatsApp}>
+                                    Chat with us
+                                </div>
+                            </div>
+
+                        </div>     
+                    </div> 
                 </div>
                
                 
@@ -80,3 +99,4 @@ const Home = () => {
 }
 
 export default Home;
+
