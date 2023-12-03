8580c34b5b0e3c2e6fdbd1885090a25b1a31d2fc
Bensolidev <77503329+Bensoli-Inc@users.noreply.github.com>
2023-12-03 07:31:58 +0300

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
                         > 
                             SENTELMO <br />
                             Engineering 
-                            <br /> Technologies
+                            <br /> Services
                         </motion.h1>
                     </div>
 
