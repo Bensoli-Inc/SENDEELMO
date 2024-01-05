f3ed6150b801356330424bd257acbc1942d1842e
Bensolidev <77503329+Bensoli-Inc@users.noreply.github.com>
2024-01-05 05:11:29 +0300

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,32 +10,29 @@ import {BsFillChatDotsFill} from 'react-icons/bs'
 const Home = () => {
     return (
             <section>
-            <div className=" innerWidth hero-container ">
-                
-                <div className="flexCenter hero-left " >
-                    <div className="hero-title title1">
-                        <motion.h1
-                        initial={{y: "2rem", opacity: 0}}
-                        animate={{y: 0, opacity: 1}}
-                        transition={{
-                            duration: 2, 
-                            type: "spring"
-                        }}
-                        > 
-                            SENTELMO <br />
-                            Electrical 
-                            <br /> Services
-                        </motion.h1>
-                    </div> 
-                    <div className="white-gradient light"/>
+                <div className='main'>
+                    <div className="flexCenter hero-left innerWidth hero-container " >
+                                <div className="hero-title title1">
+                                    <motion.h1
+                                        initial={{y: "2rem", opacity: 0}}
+                                        animate={{y: 0, opacity: 1}}
+                                        transition={{
+                                            duration: 2, 
+                                            type: "spring"
+                                        }}
+                                        > 
+                                            SENTELMO <br />
+                                            Electrical 
+                                            <br /> Services
+                                    </motion.h1>
+                                </div> 
+                                <div className="white-gradient light"/>
+                    </div>
+                   
+                    <div className='cover'>
+                        <img src="./john2.jpg" alt="cover" />
+                    </div>
                 </div>
-                <div className='cover'>  
-                                
-                        <img src="./john.jpg" alt="cover" />
-                </div>
-                
-            </div>
-            
             </section>           
     )
 }
@@ -45,4 +42,3 @@ export default Home;
                     
                     
                     
-
